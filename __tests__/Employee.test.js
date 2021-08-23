63b20ea88cada751b177a3df12a9da1d575529e5
jdwalsh138 <mysteryredstyle@gmail.com>
2021-08-23 22:04:51 +1000

    final commit

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,43 @@
+  
+const { test, expect } = require('@jest/globals');
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    describe("Employee assignment", () => {
+        it("Creates an employee objects", () => {
+            const employee = new Employee("Todd", 3, "todd@todd.com");
+           // console.log("name :" +employee.getName());
+            expect(employee.name).toEqual("Todd");
+            expect(employee.id).toEqual(3);
+            expect(employee.email).toEqual("todd@todd.com");
+        });
+
+        it("Should returns Employee", () => {
+            const employee = new Employee("Todd", 3, "todd@todd.com");
+            expect(employee.getRole()).toEqual("Employee");
+        });
+        // Exception test
+        it("should throw an error if not provided an id and email", () => {
+            const emp = new Employee("Sarah");
+            // Verify that the correct error was thrown when the callback is executed
+            expect(emp.getEmail()).toEqual(undefined);
+          });
+          // Exception test
+        it("should produce 'undefined' values if not provided any parameter values", () => {
+            const emp = new Employee();
+            expect(emp.name).toEqual(undefined);
+        });
+        // Exception test
+        it("should give typeof role if not provided any parameter values", () => {
+            const emp = new Employee();
+            let empRole;
+            empRole = emp.getRole();
+            expect(typeof empRole).toEqual("string");
+        });
+        // Exception test
+        it("should give typeof emp to be object if not provided any parameter values", () => {
+            const emp = new Employee();
+            expect(typeof emp).toEqual("object");
+        });
+    });
+});
---
+++ index.js
@@ -0,0 +1,240 @@
+// global variables
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generatePage = require('./src/html-template');
+const writeFile = require('./output/myteam.js');
+const employeeList = [];
+
+// initial questions when first initializing application
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the team manager?',
+        validate: getName => {
+            if (getName) {
+                return true;
+            } else {
+                console.log('Please enter the name of the team manager.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID of the team manager?',
+        validate: (getId) => {
+            if (isNaN(getId)) {
+                return "Please enter a valid ID.";
+            }
+            return true;
+        },
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of the team manager?',
+        validate: getEmail => {
+            if (getEmail) {
+                return true;
+            } else {
+                console.log('Please enter the email of the team manager.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is the office number of the team manager?',
+        validate: (officeNumber) => {
+            if (isNaN(officeNumber)) {
+                return "Please enter a valid office number.";
+            }
+            return true;
+        },
+    }
+];
+
+// ask what next step would be after adding Manager information
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'nextStep',
+            message: 'Which of the following steps would you like to do next?',
+            choices: [
+                "Add an engineer.",
+                "Add an intern.",
+                "Finish building my team."
+            ]
+
+        }
+    ])
+    .then(data => {
+        // go to next step depending on what the user input is
+        switch (data.nextStep) {
+            case 'Add an engineer.':
+                addEngineer();
+                break;
+            case 'Add an intern.':
+                addIntern();
+                break;
+            case 'Finish building my team.':
+                const pageHtml = generatePage(employeeList);
+                writeFile(pageHtml);
+                break;
+        }
+    }) 
+};
+
+const addEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the engineer?',
+            validate: getName => {
+                if (getName) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of the engineer.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the ID of the engineer?',
+            validate: (getId) => {
+                if (isNaN(getId)) {
+                    return "Please enter a valid ID.";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the engineer?',
+            validate: getEmail => {
+                if (getEmail) {
+                    return true;
+                } else {
+                    console.log('Please enter the email of the engineer.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: "What is the GitHub username of the engineer?",
+            validate: getGithub => {
+                if (getGithub) {
+                    return true;
+                } else {
+                    console.log('Please enter the GitHub username of the engineer.');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(data => {
+        const teamMember = new Engineer(data);
+        console.log(data);
+        console.log(teamMember.role);
+        employeeList.push(teamMember);
+        console.log(employeeList);
+        addEmployee();
+    })
+};
+
+const addIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the intern?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of the intern.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the ID of the intern?',
+            validate: (idInput) => {
+                if (isNaN(idInput)) {
+                    return "Please enter a valid ID.";
+                }
+                return true;
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the email of the intern?',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the email of the intern.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the school of the intern?",
+            validate: getSchool => {
+                if (getSchool) {
+                    return true;
+                } else {
+                    console.log('Please enter the school of the intern.');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(data => {
+        const teamMember = new Intern(data);
+        console.log(data);
+        console.log(teamMember.role);
+        employeeList.push(teamMember);
+        console.log(employeeList);
+        addEmployee();
+    })
+};
+
+// function to initialize app
+function init() {
+    return inquirer.prompt(managerQuestions);
+};
+
+init()
+    .then(data => {
+        return new Manager(data);
+    })
+    .then(data => {
+        const managerEntry = data;
+        console.log(data);
+        employeeList.push(managerEntry);
+        console.log(employeeList);
+    })
+    .then(addEmployee)
+    .catch(err => {
+        console.log(err);
+    });
